1750a28837abb78f3dc741c6b0595654a1ae4930
Amol Dhage <amoltdhage@gmail.com>
2016-11-10 02:31:21 +0530

    Initail Commit

--- ADPickerDemo/ViewController.m
+++ ADPickerDemo/ViewController.m
@@ -13,10 +13,47 @@
 @end
 
 @implementation ViewController
+@synthesize labelCity;
+
+
+
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView;
+{
+    return 1;
+}
+
+// returns the # of rows in each component..
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component;
+
+{
+    return [cityNames count];
+}
+
+- (nullable NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    return [cityNames objectAtIndex:row];
+}
+
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
+{
+    [labelCity setText:[NSString stringWithFormat:@"Selected City : %@",[cityNames objectAtIndex:row]]];
+    
+}
+
+-(IBAction)getSelectedCity:(id)sender;
+{
+
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    cityNames =[[NSArray alloc]initWithObjects:@"Mumbai",@"Pune",@"Nagpur",@"Amravati",@"Aurangabad",@"Jamod",@"Khamgaon",@"Akola",@"Satara",@"Jalgaon Jamod",@"Akola",@"Nashik", nil,nil];
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
