71fff6109c2daf8a222a95af256f038d410927a3
Amol Dhage <amoltdhage@gmail.com>
2016-11-09 23:51:53 +0530

    Initial Commit

---
+++ ADPickerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ADPickerDemo
+//
+//  Created by Amol Tukaram Dhage on 09/11/16.
+//  Copyright © 2016 AmolTDhage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
